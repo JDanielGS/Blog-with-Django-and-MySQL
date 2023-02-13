8aa9f9f6ac986c8361a67b0b0b401487ca8df012
JDanielGS <danielgachasut@gmail.com>
2023-02-13 16:59:57 -0500

    last version Blog with Django and MySQL

--- crud/urls.py
+++ crud/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('write/', views.write, name="write"),
+    path('insert/', views.insert, name="insert"),
+    path('post/<int:id>', views.post, name="post"),
 ]
